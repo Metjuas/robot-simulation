642d986000e69b371f4f2fc7964d0819fa5fecae
megy <dominik000@pobox.sk>
2024-04-14 22:53:47 +0200

    update PageSim layout

--- src/Pages/PageSim.cpp
+++ src/Pages/PageSim.cpp
@@ -25,21 +25,32 @@ PageSim::PageSim(QWidget *parent) : QWidget(parent), view(&scene, this) {
 
     timer->start(TickRate);
 
-    QPushButton *button = new QPushButton("Pause simulation", this);
-    button->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QPushButton *pause_button = new QPushButton("Pause simulation", this);
+    pause_button->setFixedHeight(50);
+    QPushButton *menu_button = new QPushButton("Menu", this);
+    menu_button->setFixedHeight(50);
+    QPushButton *go_button = new QPushButton("Go/Stop", this);
+    QPushButton *left_button = new QPushButton("<-", this);
+    QPushButton *right_button = new QPushButton("->", this);
+    //button->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
-    connect(button, &QPushButton::clicked, [=]() {
+    connect(pause_button, &QPushButton::clicked, [=]() {
         if (timer->isActive()) {
             timer->stop();
-            button->setText("Resume");
+            pause_button->setText("Resume");
         } else {
             timer->start(TickRate);
-            button->setText("Pause");
+            pause_button->setText("Pause");
         }
     });
 
+    //setLayout
     QGridLayout *gridLayout = new QGridLayout();
-    gridLayout->addWidget(button, 0, 0);
+    gridLayout->addWidget(pause_button, 0, 0, 2, 2);
+    gridLayout->addWidget(menu_button, 0, 10, 2, 2);
+    gridLayout->addWidget(go_button, 0, 4, 1, 2);
+    gridLayout->addWidget(left_button, 1, 4);
+    gridLayout->addWidget(right_button, 1, 5);
 
     QVBoxLayout *vLayout = new QVBoxLayout();
     vLayout->addWidget(&view);
